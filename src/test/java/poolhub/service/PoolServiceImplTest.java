bbfe355b0181fb08bc59172ae32582439c712e12
leiserid <dimitri.leisering@iut2.univ-grenoble-alpes.fr>
2022-09-13 18:44:52 +0200

    create skeleton test for getAllPools

--- src/test/java/poolhub/service/PoolServiceImplTest.java
+++ src/test/java/poolhub/service/PoolServiceImplTest.java
@@ -1,51 +1,81 @@
 package poolhub.service;
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import javax.mail.Multipart;
-import javax.mail.Session;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
-import org.junit.BeforeClass;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.Page;
-import org.springframework.mail.MailSendException;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 import poolhub.IntegrationTest;
-import poolhub.config.Constants;
-import poolhub.domain.User;
+import poolhub.domain.Pool;
+import poolhub.repository.PoolRepository;
 import poolhub.service.dto.PoolListResponseDto;
-import tech.jhipster.config.JHipsterProperties;
+import poolhub.service.mapper.PoolMapper;
+import poolhub.service.serviceImpl.PoolServiceImpl;
 
+@IntegrationTest
+@Transactional
 public class PoolServiceImplTest {
-    private static Page<PoolListResponseDto> pg ;
 
-    @BeforeClass
-    public void init(){
-        pg = mock(Page.class);
+    @Mock
+    private PoolRepository poolRepository;
 
+    @Mock
+    private EntityManager em;
+
+    private PoolService poolService;
+
+    private List<Pool> poolDb;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        poolService = new PoolServiceImpl(poolRepository, em);
+
+        for (int i = 0; i < 3; i++) {
+            Pool mp = new Pool();
+            mp.setId((long) i);
+            mp.setRef("a" + i);
+            mp.setLabel(i + "a" + i);
+            poolDb.add(mp);
+        }
     }
+
     @Test
-    void testgetAllPools() throws Exception {
+    void getAllPool() {
+        //set the findAll() comportment
+        int nbPage = 1;
+        int pageSize = 6;
 
+        Pageable pageable = PageRequest.of(nbPage, pageSize);
+        Page<Pool> result = new PageImpl<>(poolDb, pageable, poolDb.size()); //on search test, replace poolDb by a list containing only the results that validate the filters
+
+        when(poolRepository.findAll(pageable)).thenReturn(result);
+
+        //create the expected result
+        Page<PoolListResponseDto> expected = new PageImpl<>(
+            result.map(PoolMapper::mapToListResponse).toList(),
+            pageable,
+            result.getTotalElements()
+        );
+
+        // get the result of the method
+        Page<PoolListResponseDto> newResult = poolService.getAllPools(nbPage, pageSize);
+
+        // verify the result is equal to the expected result
+        assertEquals(expected.getTotalElements(), result.getTotalElements());
+
+        for (int i = 0; i < expected.getTotalElements(); i++) {
+            //assertTrue(expected.getContent().map(/*garder que l'id pour chaque element*/).contains(/*result[i].id*/));
+        }
     }
-
 }
