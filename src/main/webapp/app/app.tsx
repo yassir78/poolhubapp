cc93ea6f932455c502da9ff99997d86106f46be4
Telmo <telmosantosbp@hotmail.com>
2022-09-14 14:27:53 +0200

    feat: Details page design and liquibase pools data

--- src/main/webapp/app/app.tsx
+++ src/main/webapp/app/app.tsx
@@ -6,6 +6,7 @@ import { Route, Routes } from 'react-router-dom';
 import MenuPage from 'app/pages/MenuPage';
 import Footer from 'app/components/Footer';
 import setupAxiosInterceptors from 'app/helpers/services/axios-interceptor';
+import PoolDetailsPage from "app/pages/PoolDetailsPage";
 
 /*const actions = bindActionCreators({ clearAuthentication }, store.dispatch);
 setupAxiosInterceptors(() => actions.clearAuthentication('login.error.unauthorized'));*/
@@ -14,9 +15,10 @@ export const App = () => {
   return (
     <>
       <Navbar />
-      <div className="min-h-screen mt-12	bg-octonary px-24 ">
+      <div className="min-h-screen mt-12 py-16 bg-octonary px-24 ">
         <Routes>
           <Route path="/" element={<MenuPage />} />
+          <Route path="/pool" element={<PoolDetailsPage />} />
         </Routes>
       </div>
       <Footer />
---
+++ src/main/webapp/app/pages/PoolDetailsPage/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PoolDetailsContainer from "app/containers/PoolDetailsContainer";
+
+const PoolDetailsPage = () => {
+  return (
+    <PoolDetailsContainer/>
+  );
+};
+
+export default PoolDetailsPage;
